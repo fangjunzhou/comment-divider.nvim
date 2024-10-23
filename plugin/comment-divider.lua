df99b551acd5b3d02cba84591b753bdc043d4ee3
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-10-22 23:39:06 -0700

    Format the project

--- plugin/comment-divider.lua
+++ plugin/comment-divider.lua
@@ -1,19 +1,13 @@
 -- Comment divider line.
 vim.api.nvim_create_user_command("CommentDividerLine", function()
 	require("comment-divider").commentLine()
-end, {
-	nargs = 0,
-})
+end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("CommentDividerBox", function()
 	require("comment-divider").commentBox()
-end, {
-	nargs = 0,
-})
+end, { nargs = 0 })
 
 -- Check current filetype.
 vim.api.nvim_create_user_command("CommentDividerFiletype", function()
 	require("comment-divider").checkFiletype()
-end, {
-	nargs = 0,
-})
+end, { nargs = 0 })
