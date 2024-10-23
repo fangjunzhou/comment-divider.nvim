73b4244f3464db8c6a59971ceedfabdd3f6e6128
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-10-22 23:44:03 -0700

    Added runtimepath when started

--- .nvim.lua
+++ .nvim.lua
@@ -1,4 +1,5 @@
-print("Comment divider development environment setup.")
+-- Add current directory to the runtimepath.
+vim.opt.runtimepath:append(".")
 
 -- Register development reload command.
 vim.api.nvim_create_user_command("CommentDividerReload", function()
